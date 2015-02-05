3f7d816a2c1b1bceb875f0db6d4dc74280c92e09
Tunde Aderinwale <aderinwaletunde@gmail.com>
2015-02-06 00:00:15 +0200

    backv to normal ajax call new access

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ require_once("includes/component_loader.php");
             }
         header('Content-type: application/json');
        // header('Access-Control-Allow-Origin: *');
-      //  echo $_GET['callback'] . '('.json_encode($user).')';
-        echo json_encode($user);
+        echo $_GET['callback'] . '('.json_encode($user).')';
+      //  echo json_encode($user);
     	}
         
 ?>
